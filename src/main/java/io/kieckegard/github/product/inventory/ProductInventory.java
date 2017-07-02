738d7dca1d3c071d69712079c152f8babf2c0833
Pedro Arthur Fernandes de Vasconcelos <pfernandesvasconcelos@gmail.com>
2017-07-02 13:12:45 -0300

    changing package

--- src/main/java/io/kieckegard/github/product/inventory/ProductInventory.java
+++ src/main/java/io/kieckegard/github/product/inventory/ProductInventory.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package io.kieckegard.github.product.inventory.entities;
+package io.kieckegard.github.product.inventory;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
